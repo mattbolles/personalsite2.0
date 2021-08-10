9e52ec42e001bf89760dde2ac7af531e72cf10c5
Matt Bolles <mattrbolles@gmail.com>
2021-08-09 22:25:19 -0700

    Added basic project card template.

--- personal-site/src/components/ProjectCard.js
+++ personal-site/src/components/ProjectCard.js
@@ -1,11 +1,11 @@
 import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
+import { StaticImage, GatsbyImage } from "gatsby-plugin-image"
 import React from "react";
 import SectionHeading from "../components/SectionHeading";
 import { MdDevices }from '@react-icons/all-files/md/MdDevices'
 const ProjectCard = ({Project}) => {
-
-
+    const imageURL = Project.image;
 
 
 
@@ -14,6 +14,10 @@ const ProjectCard = ({Project}) => {
     <div>
         <div>{Project.name}</div>
         <div>{Project.description}</div>
+        <div>{Project.link}</div>
+        <div>{Project.technologies}</div>
+        <div><GatsbyImage image="../images/site-logo.png" alt={Project.name} /></div>
+        <div>{imageURL}</div>
     </div>
   );
 };
