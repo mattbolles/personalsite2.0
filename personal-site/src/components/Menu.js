77de4718ff815c7309cd12863c3fe1dae6e3265e
Matt Bolles <mattrbolles@gmail.com>
2021-08-20 17:27:44 -0700

    Hamburger menu finished.

--- personal-site/src/components/Menu.js
+++ personal-site/src/components/Menu.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 import { MdClose } from "@react-icons/all-files/md/MdClose"
 import { FiMenu } from "@react-icons/all-files/fi/FiMenu"
+import { graphql, useStaticQuery } from "gatsby";
 
 const Menu = () => {
+const data = useStaticQuery(graphql`
+  query MenuDataQuery {
+    allMenuJson {
+      edges {
+        node {
+            title
+            link
+            color
+        }
+      }
+    }
+  }
+`);
 
 const [menuOpen, setMenuOpen] = useState(false);
+const [selectedLink, setSelectedLink] = useState(-1);
 
 const handleMenuClick = () => {
     setMenuOpen(prev => !prev)
@@ -13,6 +28,7 @@ const handleMenuClick = () => {
 const closeMenu = () => {
     setMenuOpen(false);
 }
+
 
     return(
         <div className="menu">
@@ -24,7 +40,25 @@ const closeMenu = () => {
                 <FiMenu style = {{color: "#7b7b7b", width: "30px", height: "30px"}}/>}
                 </button>
                 <ul className={`menuNav ${menuOpen ? " showMenu" : ""}`}>
-                <a href="http://google.com" target="_blank" onClick={() => closeMenu()}>Test</a>
+                <div className = "skills">
+                    {data.allMenuJson.edges.map(({ node }, index) => (
+                        // appends showMenu to classname if open, appends selected if hovered over
+                    <div className = {`menu-link ${menuOpen ? " showMenu" : ""} ${selectedLink === index ? "selected" : ""}`}  
+                    
+                    key = {index} onMouseEnter={()=> setSelectedLink(index)} onMouseLeave={() => setSelectedLink(-1)}>
+                        <h4>
+                            
+                        {
+                            selectedLink === index ?
+                            <a href={node.link} style = {{color: node.color}} onClick = {() => closeMenu()}>{node.title}</a>
+                            :
+                            <a href={node.link} onClick = {() => closeMenu()}>{node.title}</a>
+
+                        }</h4>
+                    </div>
+                    ))}
+                </div>
+
                 </ul>
                 
             </nav>
